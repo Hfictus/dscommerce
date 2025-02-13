07023a28fe77da5309d60a2e834d024e3346b143
Gerson Klauck <gk120hf@gmail.com>
2025-02-13 15:58:48 -0300

    POST newproduct

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -5,6 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,20 +24,23 @@ public class ProductController {
 	public ProductDTO findById(@PathVariable Long id) {
 		return service.findById(id);
 	}
-	/**
-	 * Postman : productbyid
-	 * GET | http://localhost:8080/products/1
-	 * 200 ok 
-	 */
-	
+		
 	@GetMapping
 	public Page<ProductDTO> findAll(Pageable pageable) {
 		return service.findAll(pageable);
 	}
-	/**
-	 * Postman : products
-	 * GET | http://localhost:8080/products
-	 * ?size=12&page=0&sort=id,desc&name=pc gamer
-	 * 200 ok 
+	
+	
+	@PostMapping
+	public ProductDTO insert(@RequestBody ProductDTO dto) {
+		return service.insert(dto);
+	}
+	/**dto = service.insert(dto);
+	 * return dto;
+	 * ...
+	 * Postman / newproduct
+	 * POST | http://localhost:8080/products
+	 * JSON {"name": "...", "description": "...", "price": 0.0, "imgUrl": "..."} 
 	 */
+	
 }
