6e2c4e059ccd66b07363f613a895c597e99741f9
Gerson Klauck <gk120hf@gmail.com>
2025-02-15 16:04:58 -0300

    findById with try-catch

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscommerce.controllers;
 
 import java.net.URI;
+import java.time.Instant;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -16,8 +17,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.devsuperior.dscommerce.dto.CustomError;
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.services.ProductService;
+import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -26,10 +29,19 @@ public class ProductController {
 	@Autowired
 	private ProductService service;
 	
+	
+	//código de findById ao se apenas lançar a exceção em ProductService:
+	//... () -> new ResourceNotFoundException("Recurso não encontrado")... (tratar e lançar outro objeto)
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
-		ProductDTO dto = service.findById(id);
-		return ResponseEntity.ok(dto);
+	public ResponseEntity<?> findById(@PathVariable Long id) {
+		try {
+			ProductDTO dto = service.findById(id);
+			return ResponseEntity.ok(dto);
+		}
+		catch(ResourceNotFoundException e) {
+			CustomError err = new CustomError(Instant.now(), 404, e.getMessage(), "caminho");
+			return ResponseEntity.status(404).body(err);
+		}
 	}
 		
 	@GetMapping
