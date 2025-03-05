b527a61f3d4dccb1682dffc8e112f6410916c706
Gerson Klauck <gk120hf@gmail.com>
2025-03-05 12:50:50 -0300

    dependencies spring security, tests with password

--- src/main/java/com/devsuperior/dscommerce/DscommerceApplication.java
+++ src/main/java/com/devsuperior/dscommerce/DscommerceApplication.java
@@ -1,13 +1,31 @@
 package com.devsuperior.dscommerce;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DscommerceApplication {
+public class DscommerceApplication implements CommandLineRunner {
 
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DscommerceApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Encode: " + passwordEncoder.encode("123456"));
+		
+		boolean result = passwordEncoder.matches("123456", "$2a$10$1gaMn7.o1BPBOP0H3xHYSOOOq1NMPjyrjlF4XKzlD8dutVJl66cN6");
+		
+		System.out.println("123456 = Encode?: " + result);
+	}
+	
+	
+	
 }
