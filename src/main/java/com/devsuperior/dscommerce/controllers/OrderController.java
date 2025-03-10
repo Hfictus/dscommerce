440324027574dc7c5433e278956611cc42a80d62
Gerson Klauck <gk120hf@gmail.com>
2025-03-10 12:34:52 -0300

    validated client access, GET Order by id

--- src/main/java/com/devsuperior/dscommerce/controllers/OrderController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/OrderController.java
@@ -24,9 +24,8 @@ public class OrderController {
 	
 	@Autowired
 	private OrderService orderService;
-	
-	
-	@PreAuthorize("hasRole('ROLE_ADMIN')")
+		
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<OrderDTO> findById(@PathVariable Long id) {
 		OrderDTO dto = orderService.findById(id);
